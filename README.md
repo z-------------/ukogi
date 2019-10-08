cb8a41c75fe25d907d69f0352a5203dbfde7a52c
Zack Guard <zacharyguard@gmail.com>
2019-10-08 12:21:57 +0800

    Add examples

--- README.md
+++ README.md
@@ -25,6 +25,12 @@ Register a handler for an IPC channel
 | channel | <code>string</code> | Name of the channel |
 | callback | <code>function</code> | Callback to run when a message is received on this channel, called with arguments (event, arg, reply). Arguments passed to reply(arg) will be received by the callback in the corresponding `ukogi.send` call. |
 
+**Example**  
+```js
+ukogi.on("my-channel", (event, arg, reply) => {
+  if (arg.flag) reply(42);
+});
+```
 <a name="send"></a>
 
 ## send(channel, arg, callback)
@@ -38,6 +44,10 @@ Send a message through an IPC channel and receive the response
 | arg | <code>\*</code> | Argument to pass |
 | callback | <code>function</code> | Callback to run when a reply is received on this channel, called with arguments (event, arg). Arguments passed to the corresponding `ukogi.on` call's reply(arg) will be received here. |
 
+**Example**  
+```js
+ukogi.send("my-channel", { flag: true }, (event, arg) => console.log(arg)); // -> 42
+```
 
 ## License
 
--- ukogi.js
+++ ukogi.js
@@ -12,6 +12,10 @@ let listeners = {};
  * Register a handler for an IPC channel
  * @param {string} channel - Name of the channel
  * @param {function(Event, *, function(*))} callback - Callback to run when a message is received on this channel, called with arguments (event, arg, reply). Arguments passed to reply(arg) will be received by the callback in the corresponding `ukogi.send` call.
+ * @example
+ * ukogi.on("my-channel", (event, arg, reply) => {
+ *   if (arg.flag) reply(42);
+ * });
  */
 const on = (channel, callback) => {
   ipc.on(channel, (e, arg) => {
@@ -29,6 +33,8 @@ const on = (channel, callback) => {
  * @param {string} channel - Name of the channel
  * @param {*} arg - Argument to pass
  * @param {function(Event, *)} callback - Callback to run when a reply is received on this channel, called with arguments (event, arg). Arguments passed to the corresponding `ukogi.on` call's reply(arg) will be received here.
+ * @example
+ * ukogi.send("my-channel", { flag: true }, (event, arg) => console.log(arg)); // -> 42
  */
 const send = (channel, arg, callback) => {
   const id = uuid();
